98c56afe39202556e846fce80ebed3e81bc7e00a
mikailamin-master <mikailamin.freefire@gmail.com>
2026-02-13 13:04:18 +0600

    update

--- app/src/main/java/pro/magisk/RequestActivity.java
+++ app/src/main/java/pro/magisk/RequestActivity.java
@@ -1,21 +1,25 @@
 package pro.magisk;
 
 import android.os.Bundle;
-import android.view.View;
-import androidx.annotation.NonNull;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import java.io.DataOutputStream;
 import android.os.Build;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.Objects;
+
 import pro.magisk.databinding.ActivityRequestBinding;
-import com.google.android.material.slider.LabelFormatter;
-import com.google.android.material.slider.Slider;
 
 public class RequestActivity extends AppCompatActivity {
 
     private ActivityRequestBinding binding;
+    private String action;
+    private int uid = -1;
+    private int pid = -1;
+    private File output;
+    private boolean isWritten = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,53 +29,74 @@ public class RequestActivity extends AppCompatActivity {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
 	        getWindow().setBackgroundBlurRadius(15);
         }
+        if (!getIntent().hasExtra("action")) finishAndRemoveTask();
+        if (!getIntent().hasExtra("action")) return;
+        if (Objects.equals(getIntent().getStringExtra("action"), "request")) {
+            uid = getIntent().getIntExtra("uid", -1);
+        } else {
+            uid = getIntent().getIntExtra("from.uid", -1);
+        }
+        pid = getIntent().getIntExtra("pid", -1);
         String fifoPath = getIntent().getStringExtra("fifo");
-        final File output = new File(fifoPath);
+        assert fifoPath != null;
+        output = new File(fifoPath);
 
-        binding.allow.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                try {
-                    FileOutputStream fos = new FileOutputStream(output);
-                    DataOutputStream dos = new DataOutputStream(fos);
-                    dos.writeInt(2);
-                    dos.flush();
-                    dos.close();
-                    fos.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-                finish();
+        binding.allow.setOnClickListener(view -> write_response(2));
+        binding.deny.setOnClickListener(view -> write_response(1));
+        binding.untilSlider.setValue(0);
+        binding.notiSwitch.setEnabled(binding.untilSlider.getValue() != -1);
+        binding.untilSlider.setLabelFormatter(value -> {
+            if ((int) value == -1) {
+                binding.notiSwitch.setEnabled(false);
+                return " " + getResources().getString(R.string.allow_only_for_this_process) + " ";
+            } else if ((int) value == 0) {
+                if (!binding.notiSwitch.isChecked()) binding.notiSwitch.setChecked(true);
+                binding.notiSwitch.setEnabled(true);
+                return " " + getResources().getString(R.string.allow_for_forever) + " ";
+            } else {
+                if (!binding.notiSwitch.isChecked()) binding.notiSwitch.setChecked(true);
+                binding.notiSwitch.setEnabled(true);
+                return "  " + getResources().getString(R.string.allow_for) + ": (" + (int) value + ") " + getResources().getString(R.string.minute) +  " ";
             }
         });
-        binding.deny.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                try {
-                    FileOutputStream fos = new FileOutputStream(output);
-                    DataOutputStream dos = new DataOutputStream(fos);
-                    dos.writeInt(1);
-                    dos.flush();
-                    dos.close();
-                    fos.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-                finish();
+    }
+
+    @Override
+    public void onPause() {
+        write_response(1);
+        super.onPause();
+    }
+
+    private void write_response(int _policy) {
+        if (!isWritten) {
+            isWritten = true;
+            try {
+                FileOutputStream fos = new FileOutputStream(output);
+                DataOutputStream dos = new DataOutputStream(fos);
+                dos.writeInt(_policy);
+                dos.flush();
+                dos.close();
+                fos.close();
+            } catch (IOException e) {
+                Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_LONG).show();
             }
-        });
-        binding.untilSlider.setLabelFormatter(new LabelFormatter() {
-            @NonNull
-            @Override
-            public String getFormattedValue(float value) {
-                if ((int) value == -1) {
-                    return " " + getResources().getString(R.string.allow_only_for_this_process) + " ";
-                } else if ((int) value == 0) {
-                    return " " + getResources().getString(R.string.allow_for_forever) + " ";
-                }else {
-                    return " Allow for: (" + String.valueOf((int) value) + ") minute ";
-                }
-            }
-        });
+        }
+        finishAndRemoveTask();
+    }
+
+    public int getPid() {
+        return pid;
+    }
+
+    public void setPid(int pid) {
+        this.pid = pid;
+    }
+
+    public int getUid() {
+        return uid;
+    }
+
+    public void setUid(int uid) {
+        this.uid = uid;
     }
 }
